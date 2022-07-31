95cebc3cc1469b4418240c71ceb425022936342d
neogmanish99 <sanjuneog399@gmail.com>
2022-07-31 23:53:13 +0530

    worked on part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,18 @@ import React from "react";
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
 
+import {
+    siteTitle,
+    siteHeading,
+} from "../components/components/layout.module.css";
+
 const IndexPage = () => {
     return (
         <>
-            <Layout pageTitle="Home Page">
-                <p>I'm making this by following the Gatsby Tutorial.</p>
+            <Layout>
+                <p className={siteHeading}>
+                    I'm making this by following the Gatsby Tutorial.
+                </p>
                 <StaticImage
                     alt="Clifford"
                     src="../images/wallpapersden.com_hatsune-miku-half-mask_2389x1571.jpg"
